785ebb34c80f9cd99115d0c1e1686d64db8a4d3b
Rishvant Singh <rishvantsinghpundir@gmail.com>
2025-04-22 20:46:25 +0530

    refactor: clean up Master component by removing unused code and reorganizing imports

--- client/src/pages/dashboard/Master.jsx
+++ client/src/pages/dashboard/Master.jsx
@@ -1,29 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { MasterSidenav } from "@/widgets/layout";
-import {
-  createWarehouse,
-  fetchWarehouse,
-  getWarehouseById,
-} from "@/services/warehouseService";
 import { useNavigate } from "react-router-dom";
+
+// api services
+import { fetchWarehouse, getWarehouseById } from "@/services/warehouseService";
+
+// components
 import statesAndCities from "@/constants/statecities.json";
-import { toast } from "sonner";
 import ItemForm from "@/components/master/ItemForm";
 import TransportForm from "@/components/master/TransportForm";
 import WarehouseForm from "@/components/master/WarehouseForm";
 import BuyerForm from "@/components/master/BuyerForm";
 import ManufacturerForm from "@/components/master/ManufacturerForm";
+import { MasterSidenav } from "@/widgets/layout";
 
 export function Master() {
   const [selectedComponent, setSelectedComponent] = useState("warehouse");
-
-  const data = [
-    { label: "Warehouse", value: "warehouse" },
-    { label: "Add Items", value: "addItems" },
-    { label: "Add Transportation", value: "addTransportation" },
-    { label: "Add Buyer", value: "addBuyer" },
-    { label: "Add Manufacturer", value: "addManufacturer" },
-  ];
 
   const renderComponent = () => {
     switch (selectedComponent) {
@@ -89,53 +80,6 @@ export function Master() {
     fetchFilteredWarehouses();
   }, [selectedCity]);
 
-  const handleWarehouseSubmit = async () => {
-    setLoading(true);
-    if (selectedWarehouseID) {
-      toast.success(
-        "Existing warehouse selected. Proceeding with the selected warehouse."
-      );
-      const response = await getWarehouseById(selectedWarehouseID);
-      localStorage.setItem("warehouse", response._id);
-      navigate("/dashboard/orders");
-    } else {
-      toast.success("Proceeding with new warehouse creation!");
-      const response = await createWarehouse({
-        name: warehouseName,
-        location: { state: selectedState, city: selectedCity },
-      });
-      localStorage.setItem("warehouse", response.warehouse._id);
-      navigate("/orders");
-    }
-    setLoading(false);
-  };
-
-  const handleChangeWarehouse = () => {
-    setCurrentWarehouse(null);
-    setSelectedState("");
-    setSelectedCity("");
-    setWarehouseName("");
-    setSelectedWarehouseID("");
-    localStorage.removeItem("warehouse");
-  };
-
-  //  const getCurrentSectionTitle = () => {
-  //    switch (selectedComponent) {
-  //      case "warehouse":
-  //        return "Warehouse ";
-  //      case "addItems":
-  //        return "Items ";
-  //      case "addTransportation":
-  //        return "Transportation ";
-  //      case "addBuyer":
-  //        return "Buyer ";
-  //      case "addManufacturer":
-  //        return "Manufacturer ";
-  //      default:
-  //        return "Warehouse ";
-  //    }
-  //  };
-
   return (
     <div className="flex">
       <div className="fixed w-[20%] p-5">
@@ -143,13 +87,7 @@ export function Master() {
       </div>
 
       <div className="w-[80%] ml-[19%] px-5">
-        {/* <div className="bg-white rounded-lg shadow-md border-2 border-[#929292] mt-12 mb-8">
-          <h1 className="text-[1.1rem] text-[#636363] px-8 py-2 border-b-2 border-b-[#929292]">
-            {getCurrentSectionTitle()}
-            <span className="text-[1.5rem] text-black">/ Available</span>
-          </h1> */}
         <div className="p-10">{renderComponent()}</div>
-        {/* </div> */}
       </div>
     </div>
   );
